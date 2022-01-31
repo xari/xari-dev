7d3574ce0e4c207ad68ac194c7df3ee62be3647b
xari <hrtanderson@gmail.com>
2022-01-31 20:54:27 +0100

    refactored to use custom hook

--- content/blog/package-dependencies/index.js
+++ content/blog/package-dependencies/index.js
@@ -4,76 +4,44 @@ import Tree from "./tree.js"
 import ForceGraph from "./force-graph.js"
 import Legend from "./legend.js"
 
-export function TreeChart({ data }) {
+function useD3(callback, ...args) {
   const ref = React.useRef(null)
 
   React.useEffect(() => {
     if (ref.current !== null) {
       ref.current.innerHTML = "" // Prevent duplication
 
-      Tree(
-        data,
-        {
-          label: d => d.name,
-          children: d =>
-            typeof d.dependencies !== "undefined" &&
-            Object.values(d.dependencies),
-          title: (d, n) =>
-            `${n
-              .ancestors()
-              .reverse()
-              .map(d => d.data.name)
-              .join(".")}`, // hover text
-          width: 1100,
-        },
-        ref
-      )
+      callback(...args, ref)
     }
   })
 
   return <svg ref={ref}></svg>
 }
 
-export function ForceGraphChart({ data }) {
-  const ref = React.useRef(null)
-
-  React.useEffect(() => {
-    if (ref.current !== null) {
-      ref.current.innerHTML = "" // Prevent duplication
-
-      ForceGraph(
-        data,
-        {
-          nodeId: d => d.id,
-          nodeGroup: d => d.group,
-          nodeTitle: d => `${d.id}\n${d.group}`,
-          height: 460,
-        },
-        ref
-      )
-    }
+export const TreeChart = ({ data }) =>
+  useD3(Tree, data, {
+    label: d => d.name,
+    children: d =>
+      typeof d.dependencies !== "undefined" && Object.values(d.dependencies),
+    title: (d, n) =>
+      `${n
+        .ancestors()
+        .reverse()
+        .map(d => d.data.name)
+        .join(".")}`, // hover text
+    width: 1100,
   })
 
-  return <svg ref={ref}></svg>
-}
-
-export function ForceLegend({ range, title }) {
-  const ref = React.useRef(null)
-
-  React.useEffect(() => {
-    if (ref.current !== null) {
-      ref.current.innerHTML = "" // Prevent duplication
-
-      Legend(
-        d3.scaleOrdinal(range, d3.schemeTableau10),
-        {
-          title: title,
-          tickSize: 0,
-        },
-        ref
-      )
-    }
+export const ForceGraphChart = ({ data }) =>
+  useD3(ForceGraph, data, {
+    nodeId: d => d.id,
+    nodeGroup: d => d.group,
+    nodeTitle: d => `${d.id}\n${d.group}`,
+    height: 460,
   })
 
-  return <svg ref={ref}></svg>
-}
+export const ForceLegend = ({ range, title }) =>
+  useD3(Legend, d3.scaleOrdinal(range, d3.schemeTableau10), {
+    title: title,
+    tickSize: 0,
+  })
