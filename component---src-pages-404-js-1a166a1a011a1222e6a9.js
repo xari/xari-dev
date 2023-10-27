9060dccc98dee5a160a95476ffe1f5f5debb6970
Harry Robert Taylor Anderson <handerso@bbd-9sczyl3.epfl.ch>
2023-10-27 10:58:44 +0200

    Updates

---
+++ component---src-pages-404-js-1a166a1a011a1222e6a9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkxari_dev=self.webpackChunkxari_dev||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),l=a(8678),u=a(262);e.default=function(t){var e=t.data,a=t.location,i=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:i},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1a166a1a011a1222e6a9.js.map
