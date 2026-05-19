6f9ad8889518bf9d1b5942fb7d104c5661bfed44
Harry Anderson <hrtanderson@gmail.com>
2026-05-19 11:00:50 +0200

    Update layout.js to break QG!!!

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const getSemVer = function (version) {
+  const re = new RegExp(
+    "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$",
+    "gm"
+  )
+  return version.match(re) !== null ? version : "latest"
+}
+
+const getPkgDeps = async (name, version) => {
+  const { dependencies } = await fetch(
+    `https://registry.npmjs.org/${name}/${getSemVer(version)}`
+  ).then(res => res.json())
+  return {
+    name,
+    version,
+    dependencies:
+      typeof dependencies !== "undefined"
+        ? Object.fromEntries(
+            await Promise.all(
+              Object.entries(dependencies).map(async ([k, v]) => [
+                k,
+                await getPkgDeps(k, v),
+              ])
+            )
+          )
+        : dependencies,
+  }
+}
+
+const treeChart = Tree(npmPkg, {
+  label: d => d.name,
+  children: d =>
+    typeof d.dependencies !== "undefined" && Object.values(d.dependencies),
+Compare with `undefined` directly instead of using `typeof`.
+
+  title: (d, n) =>
+    `${n
+      .ancestors()
+      .reverse()
+      .map(d => d.data.name)
+      .join(".")}`, // hover text
+  width: 1152,
+})
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
