3289a76561bede263779ed2e0a70fff94df8a903
Harry Anderson <hrtanderson@gmail.com>
2026-05-19 10:56:43 +0200

    Apply suggestion from @xari

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 const testRemediationAgent = 123;
-
+const getSemVer = function (version) {
+  const re = new RegExp(
+    "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$",
+    "gm"
+  )
+  return version.match(re) !== null ? version : "latest"
+}
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
